626ee039701f277832e601918cb1a455e700f1a6
liamduffy <lduffy@voysis.com>
2018-06-13 17:21:22 +0100

    removed optional from callback.

--- Voysis/events/CallbackDispatcher.swift
+++ Voysis/events/CallbackDispatcher.swift
@@ -2,7 +2,7 @@ import Foundation
 
 internal class CallbackDispatcher<T: Callback> {
     private let dispatchQueue: DispatchQueue
-    private weak var callback: T?
+    private weak var callback: T!
 
     init(_ dispatchQueue: DispatchQueue, _ callback: T) {
         self.dispatchQueue = dispatchQueue
@@ -11,37 +11,37 @@ internal class CallbackDispatcher<T: Callback> {
 
     func success(_ response: StreamResponse<T.C, T.E>) {
         dispatchQueue.async {
-            self.callback?.success(response: response)
+            self.callback.success(response: response)
         }
     }
 
     func failure(_ error: VoysisError) {
         dispatchQueue.async {
-            self.callback?.failure(error: error)
+            self.callback.failure(error: error)
         }
     }
 
     func recordingStarted() {
         dispatchQueue.async {
-            self.callback?.recordingStarted()
+            self.callback.recordingStarted()
         }
     }
 
     func queryResponse(_ queryResponse: QueryResponse) {
         dispatchQueue.async {
-            self.callback?.queryResponse(queryResponse: queryResponse)
+            self.callback.queryResponse(queryResponse: queryResponse)
         }
     }
 
     func recordingFinished(_ reason: FinishedReason) {
         dispatchQueue.async {
-            self.callback?.recordingFinished(reason: reason)
+            self.callback.recordingFinished(reason: reason)
         }
     }
 
     func audioData(_ data: Data) {
         dispatchQueue.async {
-            self.callback?.audioData(data: data)
+            self.callback.audioData(data: data)
         }
     }
 }
