d053033d5d93ae7d729b4a0a48875058c723ca85
liamduffy <lduffy@voysis.com>
2018-06-29 16:38:40 +0100

    added all argument constructors to Request and Response objects

--- Voysis/models/response/SocketResponse.swift
+++ Voysis/models/response/SocketResponse.swift
@@ -9,14 +9,27 @@ internal struct Response<T: ApiResponse>: Codable {
 
 public struct Audio: Codable {
     public let href: String?
+
+    public init(href: String) {
+        self.href = href
+    }
 }
 
 public struct Reply: Codable {
     public let text: String?
+
+    public init(text: String?) {
+        self.text = text
+    }
 }
 
 public struct Links: Codable {
     public let linksSelf, audio: Audio?
+
+    public init(linksSelf: Audio?, audio: Audio?) {
+        self.linksSelf = linksSelf
+        self.audio = audio
+    }
 
     enum CodingKeys: String, CodingKey {
         case linksSelf = "self"
@@ -47,6 +60,30 @@ public struct StreamResponse<C: Context, E: Entities>: ApiResponse, Codable {
     public let entities: E?
     public let context: C?
     public let _links: Links?
+
+    public init(id: String?,
+                locale: String?,
+                conversationId: String?,
+                queryType: String?,
+                audioQuery: AudioQuery?,
+                textQuery: Reply?,
+                intent: String?,
+                reply: Reply?,
+                entities: E?,
+                context: C?,
+                _links: Links?) {
+        self.id = id
+        self.locale = locale
+        self.conversationId = conversationId
+        self.queryType = queryType
+        self.audioQuery = audioQuery
+        self.textQuery = textQuery
+        self.intent = intent
+        self.reply = reply
+        self.entities = entities
+        self.context = context
+        self._links = _links
+    }
 }
 
 public struct Token: ApiResponse, Codable {
