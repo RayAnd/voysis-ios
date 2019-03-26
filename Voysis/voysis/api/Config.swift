01a7ca3c4636b91ac46676d1468289795460d66e
liamduffy <lduffy@voysis.com>
2019-03-26 14:57:36 +0000

    added comment

--- Voysis/voysis/api/Config.swift
+++ Voysis/voysis/api/Config.swift
@@ -16,5 +16,13 @@ public protocol Config {
 
     var audioRecordParams: AudioRecordParams? { get }
 
+    /**
+      Voice Activity Detection will automatically detect when the user stops speaking and
+      process the audio request.
+
+      Note: setting this to true will cause the client to use a webSocket as opposed to a rest
+      interface under the hood
+      @return isVadEnabled boolean. default set to true.
+     */
     var isVadEnabled : Bool { get }
 }
